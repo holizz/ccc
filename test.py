1495d39395581430e4dbda91b872f5cd00c3dc53
Tom Adams <tom@holizz.com>
2009-07-02 12:15:09 +0100

    Tests use new format schema.

--- test.py
+++ test.py
@@ -15,86 +15,86 @@ from exception import *
 
 class ArgsTest(unittest.TestCase):
     def testCreateWithNoSchemaOrArguments(self):
-        args = Args("", [])
+        args = Args([], [])
         self.assertEquals(0, args.nextArgument())
 
     def testWithNoSchemaButWithOneArgument(self):
-        self.assertRaises(ArgsException, lambda: Args("", ["-x"]))
+        self.assertRaises(ArgsException, lambda: Args([], ["-x"]))
 
     def testWithNoSchemaButWithMultipleArguments(self):
-        self.assertRaises(ArgsException, lambda: Args("", ["-x", "-y"]))
+        self.assertRaises(ArgsException, lambda: Args([], ["-x", "-y"]))
 
     def testNonLetterSchema(self):
-        self.assertRaises(ArgsException, lambda: Args("*", []))
+        self.assertRaises(ArgsException, lambda: Args([7], []))
 
     def testInvalidArgumentFormat(self):
-        self.assertRaises(ArgsException, lambda: Args("f~", []))
+        self.assertRaises(ArgsException, lambda: Args([('no type')], []))
 
 
     def testSimpleBooleanPresent(self):
-        args = Args("x", ["-x"])
+        args = Args([('x',bool)], ["-x"])
         self.assertEquals(True, args.getBoolean('x'))
         self.assertEquals(1, args.nextArgument())
 
 
     def testSimpleStringPresent(self):
-        args = Args("x*", ["-x", "param"])
+        args = Args(['x', str], ["-x", "param"])
         self.assertTrue(args.has('x'))
         self.assertEquals("param", args.getString('x'))
         self.assertEquals(2, args.nextArgument())
 
     def testMissingStringArgument(self):
-        self.assertRaises(ArgsException, lambda: Args("x*", ["-x"]))
+        self.assertRaises(ArgsException, lambda: Args([('x',str)], ["-x"]))
 
 
     def testSpacesInFormat(self):
-        args = Args("x, y", ["-xy"])
+        args = Args([('x',bool),('y',bool)], ["-xy"])
         self.assertTrue(args.has('x'))
         self.assertTrue(args.has('y'))
         self.assertEquals(1, args.nextArgument())
 
     def testSimpleIntPresent(self):
-        args = Args("x#", ["-x", "42"])
+        args = Args([('x',int)], ["-x", "42"])
         self.assertTrue(args.has('x'))
         self.assertEquals(42, args.getInt('x'))
         self.assertEquals(2, args.nextArgument())
 
     def testInvalidInteger(self):
-        self.assertRaises(ArgsException, lambda: Args("x#", ["-x", "Forty two"]))
+        self.assertRaises(ArgsException, lambda: Args([('x',int)], ["-x", "Forty two"]))
 
 
     def testMissingInteger(self):
-        self.assertRaises(ArgsException, lambda: Args("x#", ["-x"]))
+        self.assertRaises(ArgsException, lambda: Args([('x',int)], ["-x"]))
 
     def testSimpleDoublePresent(self):
-        args = Args("x##", ["-x", "42.3"])
+        args = Args([('x',float)], ["-x", "42.3"])
         self.assertTrue(args.has('x'))
         self.assertEquals(42.3, args.getDouble('x'), .001)
 
     def testInvalidDouble(self):
-        self.assertRaises(ArgsException, lambda: Args("x##", ["-x", "Forty two"]))
+        self.assertRaises(ArgsException, lambda: Args([('x',float)], ["-x", "Forty two"]))
 
     def testMissingDouble(self):
-        self.assertRaises(ArgsException, lambda: Args("x##", ["-x"]))
+        self.assertRaises(ArgsException, lambda: Args([('x',float)], ["-x"]))
 
     def testStringArray(self):
-        args = Args("x[*]", ["-x", "alpha"])
+        args = Args([('x',list)], ["-x", "alpha"])
         self.assertTrue(args.has('x'))
         result = args.getStringArray('x')
         self.assertEquals(1, len(result))
         self.assertEquals("alpha", result[0])
 
     def testMissingStringArrayElement(self):
-        self.assertRaises(ArgsException, lambda: Args("x[*]", ["-x"]))
+        self.assertRaises(ArgsException, lambda: Args([('x',list)], ["-x"]))
 
     def testExtraArguments(self):
-        args = Args("x,y*", ["-x", "-y", "alpha", "beta"])
+        args = Args([('x',bool),('y',str)], ["-x", "-y", "alpha", "beta"])
         self.assertTrue(args.getBoolean('x'))
         self.assertEquals("alpha", args.getString('y'))
         self.assertEquals(3, args.nextArgument())
 
     def testExtraArgumentsThatLookLikeFlags(self):
-        args = Args("x,y", ["-x", "alpha", "-y", "beta"])
+        args = Args([('x',bool),('y',bool)], ["-x", "alpha", "-y", "beta"])
         self.assertTrue(args.has('x'))
         self.assertFalse(args.has('y'))
         self.assertTrue(args.getBoolean('x'))
